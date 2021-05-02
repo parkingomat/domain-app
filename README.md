f1a5017f92b8e5f177cf5a9a58f6358aa01c0fee
tom <tom@sapletta.com>
2021-05-02 17:10:55 +0200

    first commit for domain app html static content

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Frontend app to list all domains in table with params
 
 https://domain-app.parkingomat.pl
+
+# GITHUB
+https://github.com/parkingomat/domain-app.git
---
+++ meta.php
@@ -0,0 +1,18 @@
+<?php
+require("load_func.php");
+
+load_func(['https://php.defjson.com/def_json.php'], function () {
+
+    def_json('meta.json', [
+        "in" => [
+            "post" => "domain_list",
+        ],
+        "function" => [
+            "description" => "get DNS info about domain list from in->file and save to out->file",
+            "include" => []
+        ],
+        "out" => [
+            "db" => "domains",
+        ],
+    ]);
+});
